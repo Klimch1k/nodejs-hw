4325ffb36f6fc5ba8642fbda1756ced99dba69d1
Klimch1k <114351322+Klimch1k@users.noreply.github.com>
2023-08-15 20:24:23 +0300

    tasks 1-4

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -2,10 +2,12 @@ const singUp = require("./singUp");
 const login = require("./login");
 const getCurrentUser = require("./getCurrentUser")
 const logout = require("./logout")
+const updateAvatar = require("./updateAvatar")
 
 module.exports = {
   singUp,
   login,
   getCurrentUser,
   logout,
+  updateAvatar
 };
